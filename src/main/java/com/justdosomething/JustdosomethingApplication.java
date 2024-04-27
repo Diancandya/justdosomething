1c7c173d8931d1d56e9d45256b13ed6d8d6394c6
Diancandya <101170871+Diancandya@users.noreply.github.com>
2024-04-28 01:46:30 +0800

    Initial commit

---
+++ src/main/java/com/justdosomething/JustdosomethingApplication.java
@@ -0,0 +1,13 @@
+package com.justdosomething;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustdosomethingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustdosomethingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/justdosomething/JustdosomethingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justdosomething;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustdosomethingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
